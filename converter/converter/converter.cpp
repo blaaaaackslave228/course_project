da9caf0bee85d785465fb7c37e438d0b7360af11
Denis <denismmnk@gmail.com>
2023-12-11 22:50:09 +0300

    added switch and cases in to the code

--- converter/converter/converter.cpp
+++ converter/converter/converter.cpp
@@ -37,33 +37,41 @@ int utf8(wstring symbol) {
 
 int bin(wstring symbol) {
     for (wchar_t ch : symbol) {
-        char tmp[33];
-        _itoa_s(static_cast<int>(ch), tmp, 33, 2);
-        wcout << L"Код символа в BIN: " << tmp;
-        wcout << endl;
+        if (ch != L' ' && ch != L'\n' && ch != L'\t') 
+        { 
+            char tmp[33];
+            _itoa_s(static_cast<int>(ch), tmp, 33, 2);
+            wcout << L"Бинарный код символа " << ch << ": " << tmp;
+            wcout << endl;
+        }
     }
     return 0;
 }
 
 int utf16(wstring symbol) {
     for (wchar_t ch : symbol) {
-        wcout << L"UTF-16 код символа " << ch << ": ";
-        if (ch >= 0x10000) {
-            wcout << hex << ((ch - 0x10000) / 0x400 + 0xD800) << ' ' << (((ch - 0x10000) % 0x400) + 0xDC00);
+        if (ch != L' ' && ch != L'\n' && ch != L'\t') 
+        {
+            wcout << L"UTF-16 код символа " << ch << ": ";
+            if (ch >= 0x10000) {
+                wcout << hex << ((ch - 0x10000) / 0x400 + 0xD800) << ' ' << (((ch - 0x10000) % 0x400) + 0xDC00);
+            }
+            else {
+                wcout << hex << static_cast<int>(ch);
+            }
+            wcout << endl;
         }
-        else {
-            wcout << hex << static_cast<int>(ch);
-        }
-        wcout << endl;
     }
     return 0;
 }
 
 int dec(wstring symbol) {
     for (wchar_t ch : symbol) {
-        wcout << L"DEC код символа " << ch << ": ";
-        wcout << static_cast<int>(ch);
-        wcout << endl;
+        if (ch != L' ' && ch != L'\n' && ch != L'\t'){
+            wcout << L"Код в Windows-1251 символа " << ch << ": ";
+            wcout << static_cast<int>(ch);
+            wcout << endl;
+        }
     }
     return 0;
 }
@@ -73,12 +81,37 @@ int main()
     setlocale(LC_ALL, "Russian");
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(1251);
-    wstring symbol;
-    wcout << L"Введите символы: ";
-    getline(wcin, symbol);
-    utf8(symbol);
-    bin(symbol);
-    utf16(symbol);
-    dec(symbol);
-    return 0;
+    int choice;
+    do
+    {
+        cout << "Выберите вариант работы с конвертером: " << endl;
+        cout << "1 - вывод кодировок символов и кодов в системах счисления" << endl;
+        cout << "2 - вывод символа по кодировке или по коду в системе счисл" << endl;
+        cout << "3 - выход из программы" << endl;
+        cin >> choice;
+        cin.ignore();
+        switch (choice)
+        {
+        case 1: {
+            wstring symbol;
+            wcout << L"Введите символы: ";
+            getline(wcin, symbol);
+            utf8(symbol);
+            bin(symbol);
+            utf16(symbol);
+            dec(symbol);
+        }
+              break;
+        case 2: {
+        }
+              break;
+        case 3:
+            break;
+        }
+    }
+        while (choice != 3);
+        return 0;
 }
+    
+
+    
